590e4374c201c58de96c375e29d530e157b498d2
danielz0102 <danyzglez@hotmail.com>
2025-06-07 19:19:40 -0600

    fix(booksModel): complete 'add' method
    In addition to inserting the book into the 'books' table, also is needed
    to insert it in the junction table along with its genres.

--- models/booksModel.js
+++ models/booksModel.js
@@ -20,13 +20,28 @@ class BooksModel {
   }
 
   async add(book) {
-    const { title, author, description, pages, publishedDate } = book
+    const { title, author, description, pages, publishedDate, genresIds } = book
     const query =
       'INSERT INTO books (title, author, description, pages, published_date) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id'
     const values = [title, author, description, pages, publishedDate]
 
     const { rows } = await handleDbError(() => db.query(query, values))
-    return rows[0].id
+    const bookId = rows[0].id
+
+    // Also insert into the junction table
+    if (genresIds.length > 0) {
+      const genreValues = genresIds.map((genreId) => [bookId, genreId])
+      // All values are inserted in a single query
+      const valuePlaceholders = genreValues
+        .map((_, i) => `($${i * 2 + 1}, $${i * 2 + 2})`)
+        .join(', ')
+      const flatValues = genreValues.flat()
+      const genreQuery = `INSERT INTO books_genres (book_id, genre_id) VALUES ${valuePlaceholders}`
+
+      await handleDbError(() => db.query(genreQuery, flatValues))
+    }
+
+    return bookId
   }
 
   async update(id, book) {
@@ -40,7 +55,7 @@ class BooksModel {
   }
 
   async delete(id) {
-    // First, delete any associations in the junction table if needed
+    // First, delete any associations in the junction table
     await handleDbError(() =>
       db.query('DELETE FROM books_genres WHERE book_id = $1', [id]),
     )
