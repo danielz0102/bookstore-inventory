15237df4afb879925f6e1ba9f4e0c7c291fbba60
danielz0102 <danyzglez@hotmail.com>
2025-06-10 17:39:34 -0600

    refactor: move uploads to public directory
    This allows express.static to serve the images.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,24 +1,15 @@
-import fs from 'node:fs/promises'
 import asyncHandler from 'express-async-handler'
 import BooksModel from '../models/booksModel.js'
 
 export const renderIndexPage = asyncHandler(async (req, res) => {
   const lastBooks = await BooksModel.getLast(9)
 
-  const lastBooksMapped = await Promise.all(
-    lastBooks.map(async (book) => {
-      const imgSrc = await fs.readFile(`uploads/${book.cover_path}`, {
-        encoding: 'base64',
-      })
-
-      return {
-        id: book.id,
-        imgSrc,
-        title: book.title,
-        author: book.author,
-      }
-    }),
-  )
+  const lastBooksMapped = lastBooks.map((book) => ({
+    id: book.id,
+    imgSrc: book.cover_path,
+    title: book.title,
+    author: book.author,
+  }))
 
   res.render('index', {
     title: 'Bookstore inventory',
